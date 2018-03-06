92dbb01d3defee788f2202ce50642e982594aff4
PeteCoward <peter@catalpa.io>
2018-03-06 14:07:48 +0900

    Update to catalpa makemessaagesplus

--- MakemessagesPlusTestProject/MakemessagesPlusTestProject/settings.py
+++ MakemessagesPlusTestProject/MakemessagesPlusTestProject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'translated_content',
-    'pofile_diff_reduce',
+    'makemessagesplus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
